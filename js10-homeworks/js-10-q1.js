6397549862399cabe26f91296f23cedec11099b1
mahdi niki <mahdi.nikgoftar@gmail.com>
2024-12-02 19:26:20 +0330

    don

--- js10-homeworks/js-10-q1.js
+++ js10-homeworks/js-10-q1.js
@@ -1,1 +1,36 @@
 // Using an example, explain the difference between block,global and function scope in variables.
+
+//Global scope means variables are accessible from anywhere in the code. Function scope means variables are accessible only within the function they are declared in. Block scope means variables are accessible only within the block (e.g., within {}) they are declared in
+
+const idk = "global";
+
+console.log(idk); //We have access
+
+
+
+
+
+
+
+function scope() {
+  const scop = "this is function scope";
+  console.log(scop);   //We have access
+}
+
+scope()
+//console.log(scop)  We do not have direct access
+
+
+
+
+
+
+function block() {
+  // console.log(block1)  We do not have  access
+  if (true) {
+    const block1 = "this is block";
+    console.log(block1); //We have access
+  }
+}
+//console.log(block1)  We do not have direct access
+block();
--- js10-homeworks/js-10-q3.js
+++ js10-homeworks/js-10-q3.js
@@ -1,1 +1,8 @@
 // According to your js10 session, write a code that shows why we shouldn't use var?
+
+if (true) {
+  var idk3 = 12345;
+  let idk2 = 1234567;
+}
+console.log(idk3); // 12345, even though idk3 was declared inside the if block  and this issue causes problems and bug
+console.log(idk2); // idk2 is not defined
